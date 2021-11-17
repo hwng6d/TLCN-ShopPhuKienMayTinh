ba6bf0563c21b239b943aa17b351acc9ac9d8a20
Hung <lephuochung6d@gmail.com>
2021-11-17 07:23:17 +0700

    successful mailing function

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const sendEmail = require('./../utils/email');
+const Email = require('./../utils/email');
 
 //create token for user signed up or logged in
 const signToken = (id, name, email) => {
@@ -44,6 +44,9 @@ exports.signup = catchAsync(async (req, res, next) => {
 	//const newUser = await User.create(req.body);
 	//Use this to prevent users try to register as a admin in role
 	const newUser = await User.create(req.body);
+
+	const url = `${req.protocol}://${req.get('host')}/api/v1/products`;
+	await new Email(newUser, url).sendWelcome();
 
 	createSendToken(newUser, 201, res);
 });
@@ -139,18 +142,18 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 	await user.save();
 
 	// 3) send it to user's email
-	const resetURL = `${req.protocol}://${req.get(
-		'host'
-	)}/api/v1/users/resetPassword/${resetToken}`;
-
-	const message = `Forgot your password?\nSubmit a PATCH request with your new password and password confirm to ${resetURL}\nIf you didn't forget your password, please ignore this email`;
-
+	// const message = `Forgot your password?\nSubmit a PATCH request with your new password and password confirm to ${resetURL}\nIf you didn't forget your password, please ignore this email`;
 	try {
-		await sendEmail({
-			email: user.email,
-			subject: 'Your password reset token (valid for 10 mins)',
-			message,
-		});
+		const resetURL = `${req.protocol}://${req.get(
+			'host'
+		)}/api/v1/users/resetPassword/${resetToken}`;
+
+		// await sendEmail({
+		// 	email: user.email,
+		// 	subject: 'Your password reset token (valid for 10 mins)',
+		// 	message,
+		// });
+		await new Email(user, resetURL).sendPasswordReset();
 
 		res.status(200).json({
 			status: 'success',
